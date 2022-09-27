661249df341a539bf706b82eabd4310b973f7e72
Daniel Lauzon <daniel.lauzon@gmail.com>
2022-09-27 14:22:17 -0400

    Lint yargs types

--- packages/validate/src/main.ts
+++ packages/validate/src/main.ts
@@ -3,15 +3,17 @@ import { hideBin } from 'yargs/helpers';
 
 const defaultRootPath = '/Volumes/Space/archive/media/audiobooks';
 
-const argv = yargs(hideBin(process.argv)).option('rootPath', {
-  alias: 'r',
-  type: 'string',
-  demandOption: true,
-  default: defaultRootPath,
-  describe: 'Path of the root directory to search from',
-}).argv;
+const argv = yargs(hideBin(process.argv))
+  .option('rootPath', {
+    alias: 'r',
+    type: 'string',
+    demandOption: true,
+    default: defaultRootPath,
+    describe: 'Path of the root directory to search from',
+  })
+  .parseSync();
+
 // destructure arguments
-// @ts-ignore
 const { rootPath: unverifiedRootPath } = argv;
 // clean the root path by removing trailing slash
 const rootPath = unverifiedRootPath.replace(/\/$/, '');
