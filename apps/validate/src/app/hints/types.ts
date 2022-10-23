32c62a48423664a7e455416b85049620d58e3437
Daniel Lauzon <daniel.lauzon@gmail.com>
2022-10-23 17:12:51 -0400

    Hints have been typed

---
+++ apps/validate/src/app/hints/types.ts
@@ -0,0 +1,12 @@
+type AuthorTitleHintReason = 'hint' | 'unique' | 'empty' | 'multiple'
+
+export interface Hint {
+  [key: string]: string | [string, AuthorTitleHintReason] | string[]
+  author?: [string, AuthorTitleHintReason]
+  title?: [string, AuthorTitleHintReason]
+  '// multiple authors'?: string[]
+  '// multiple titles'?: string[]
+  asins?: string[]
+  skip?: 'no audio files' | 'not on audible' | 'multiple authors'
+  '// Invalid author or title'?: 'FIX NOW!'
+}
