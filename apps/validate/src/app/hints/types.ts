67c0d44eed22689d52f19f28fb00c87ec806b20b
Daniel Lauzon <daniel.lauzon@gmail.com>
2022-10-23 17:41:11 -0400

    remotve generic key from Hint type

--- apps/validate/src/app/hints/types.ts
+++ apps/validate/src/app/hints/types.ts
@@ -1,11 +1,12 @@
 type AuthorTitleHintReason = 'hint' | 'unique' | 'empty' | 'multiple'
 
 export interface Hint {
-  [key: string]: string | [string, AuthorTitleHintReason] | string[]
+  // [key: string]: string | [string, AuthorTitleHintReason] | string[]
   author?: [string, AuthorTitleHintReason]
   title?: [string, AuthorTitleHintReason]
   '// multiple authors'?: string[]
   '// multiple titles'?: string[]
+  '// duration'?: string
   asins?: string[]
   skip?: 'no audio files' | 'not on audible' | 'multiple authors'
   '// Invalid author or title'?: 'FIX NOW!'
