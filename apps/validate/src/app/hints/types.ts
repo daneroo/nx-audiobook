966f5bddb819498d64947c9dc0e855d09324369d
Daniel Lauzon <daniel.lauzon@gmail.com>
2022-10-23 19:12:50 -0400

    Fixed remaining missing hints (cosmere)

--- apps/validate/src/app/hints/types.ts
+++ apps/validate/src/app/hints/types.ts
@@ -1,13 +1,12 @@
-type AuthorTitleHintReason = 'hint' | 'unique' | 'empty' | 'multiple'
+type AuthorTitleHintReason = 'hint' | 'unique' | 'multiple'
 
 export interface Hint {
   // [key: string]: string | [string, AuthorTitleHintReason] | string[]
   author?: [string, AuthorTitleHintReason]
   title?: [string, AuthorTitleHintReason]
   '// multiple authors'?: string[]
-  '// multiple titles'?: string[]
+  // '// multiple titles'?: string[]
   '// duration'?: string
   asins?: string[]
   skip?: 'no audio files' | 'not on audible' | 'multiple authors'
-  '// Invalid author or title'?: 'FIX NOW!'
 }
