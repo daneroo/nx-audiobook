045a6e178fddb70477fe8da7d08e85d1e10a0a10
Daniel Lauzon <daniel.lauzon@gmail.com>
2022-10-26 01:59:46 -0400

    added time/durationToHMS

--- packages/time/src/format.spec.ts
+++ packages/time/src/format.spec.ts
@@ -1,10 +1,26 @@
-import { describe, expect, it } from 'vitest'
-import { formatElapsed } from './format'
+import { describe, expect, test, it } from 'vitest'
+import { formatElapsed, durationToHMS } from './format'
 
 describe('formatElapsed', () => {
   it('should format a zero duration', () => {
     // this might be brittle as formatElapsed references the current time (+new Date())
     const startMs = +new Date()
     expect(formatElapsed(startMs)).toEqual('0.000s')
+  })
+})
+
+describe('durationToHMS', () => {
+  it('should format 0s', () => {
+    expect(durationToHMS(0)).toEqual('0s')
+  })
+  test.each([
+    [0, '0s'],
+    [1, '1s'],
+    [59, '59s'],
+    [75, '1m15s'],
+    [3666, '1h1m6s'],
+    [86401, '24h0m1s'],
+  ])('%# durationToHMS %p: %p', (seconds, expected) => {
+    expect(durationToHMS(seconds)).toBe(expected)
   })
 })
--- packages/time/src/format.ts
+++ packages/time/src/format.ts
@@ -4,3 +4,26 @@ export function formatElapsed(startMs: number): string {
   const elapsedSeconds = ((+new Date() - startMs) / 1000).toFixed(3)
   return elapsedSeconds + 's'
 }
+
+/**
+ * TODO Consider other options
+ * - use String.padStart
+ * - consider use in chapters.yml
+ * - constant width seconds and minutes (if no leading 0)
+ * - consider using ':' as separator
+ * - we don't want days, max unit is hours, even above 24h
+ * - fractional seconds?
+ */
+export function durationToHMS(seconds: number): string {
+  // assume seconds is an integer > 0
+  const h = Math.floor(seconds / 3600)
+  const m = Math.floor((seconds % 3600) / 60)
+  const s = Math.floor(seconds % 60)
+  if (h > 0) {
+    return `${h}h${m}m${s}s`
+  }
+  if (m > 0) {
+    return `${m}m${s}s`
+  }
+  return `${s}s`
+}
