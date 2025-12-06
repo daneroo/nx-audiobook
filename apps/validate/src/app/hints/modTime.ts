f34387228032182b980be07ce3fd349e3cfcc679
Test User <test@example.com>
2025-12-06 15:51:40 -0500

    feat: add book entry to modTimeDB

--- apps/validate/src/app/hints/modTime.ts
+++ apps/validate/src/app/hints/modTime.ts
@@ -68,7 +68,8 @@ mod_time_utc() {
 
 // prettier-ignore
 export const modTimeDB: Record<string, [string, string]> = {
-  // 2025-12-05T17:51:10Z
+  // current time
+  "Helen Thompson - Disorder: Hard Times in the 21st Century": ["2025-12-06T19:29:15Z", "NEW BOOK"],
   "Leslie Valiant - The Importance of Being Educable: A New Theory of Human Uniqueness": ["2025-12-06T05:51:48Z", "NEW BOOK"],
   "Rosemary Kirstein - The Outskirter's Secret": ["2025-12-05T17:51:10Z", "NEW BOOK"],
   "Emad Mostaque - The Last Economy: A Guide to the Age of Intelligent Economics": ["2025-11-29T11:57:45Z", "NEW BOOK"],
